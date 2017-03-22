8735ff951ea5e50590160db6c13b1e104f855780
David Barros Piedra <radivadi@gmail.com>
2017-03-22 00:24:09 -0500

    ahora captura el stream y lo muestra en mplayer. muy lento.

--- stream.sh
+++ stream.sh
@@ -1,5 +1,3 @@
 echo "Iniciando Streaming"
 adb shell wm size
-
-
-adb shell screenrecord --output-format raw-frames - > video.raw
+adb shell screenrecord --size 1280x720 --bit-rate 500000  --output-format raw-frames - | mplayer -demuxer rawvideo -rawvideo w=1280:h=720:format=rgb24 -
