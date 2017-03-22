524fa10376833c531ea2c85a3ee110471bf428df
David Barros Piedra <radivadi@gmail.com>
2017-03-22 08:13:06 -0500

    mejorando fps

--- stream.sh
+++ stream.sh
@@ -1,3 +1,3 @@
 echo "Iniciando Streaming"
 adb shell wm size
-adb shell screenrecord --size 1280x720 --bit-rate 500000  --output-format h264 - | mplayer -demuxer h264es -fps 30 -
+adb shell screenrecord --size 1280x720 --bit-rate 500000  --output-format h264 - | mplayer -nocache -demuxer h264es -fps 60 -
